0edd0838305a5da1a75861fdee0c43fa9f5e53cc
gwon <53310111+gwon522@users.noreply.github.com>
2020-09-25 00:18:31 +0900

    시큐리티 추가 마무리해야됨

--- src/main/java/org/jingwon/megabox/controller/JoinusController.java
+++ src/main/java/org/jingwon/megabox/controller/JoinusController.java
@@ -1,6 +1,8 @@
 package org.jingwon.megabox.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.AllArgsConstructor;
@@ -11,5 +13,10 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/joinus/*")
 @AllArgsConstructor
 public class JoinusController {
-
+	
+	@GetMapping("/login.do")
+	public void login(String error, String logout, Model model) {
+		
+	}
+	
 }
